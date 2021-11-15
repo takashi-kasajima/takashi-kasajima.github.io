bf7dfeb9d5c9d0e2a7baba1cd24026c0c974f1b8
takashi-kasajima <takashi-kasajima@users.noreply.github.com>
2021-11-15 09:07:31 +0000

    deploy: 32a4488a31676b3501d4a71d5e86fe8897d034f3

---
+++ _next/static/aUm5nhVfuWMkGmBLm3APw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/54-c20fc3f7a88161b86e05.js","static/chunks/pages/index-71bbd051c0314cf3ce62.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
