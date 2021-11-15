9f53f25bae3ef55c91ff319a6a6d6a4091a0b68d
kaserj <kaserj1119@gmail.com>
2021-11-15 11:30:54 +0900

    feat: atoms

---
+++ components/atoms/Colors/index.ts
@@ -0,0 +1,49 @@
+export const Colors = {
+  primary: {
+    default: "#308ccc",
+    dark: '#0b427a',
+  },
+  secondary: {
+    default: "#2A2A2D",
+    1000: "#2A2A2D",
+    800: "#343438",
+    600: "#72727A",
+    300: "#BABAC7",
+    200: "#D1D1DE",
+    50: "#F0F0FF",
+  },
+  teritary: {
+    default: "#29CC97",
+  },
+  success: {
+    default: "#29CC97",
+  },
+  danger: {
+    default: "#DE4346",
+  },
+  medium: {
+    default: "#72727A",
+  },
+  light: {
+    default: "#D1D1DE",
+  },
+  dark: {
+    default: "#2A2A2D",
+  },
+  bg: {
+    default: "#F5F6F8",
+    secondary: "#292929",
+  },
+  footerBg: {
+    default: "#1B1B1B",
+  },
+  missionBg: {
+    default: "#F9F3EA",
+  },
+  base: {
+    default: "#A0A0A0",
+  },
+  disable: {
+    default: "#4F4F4F",
+  },
+};
